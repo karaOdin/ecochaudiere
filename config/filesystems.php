4b68d52ef2b7f8ecc629c06fc082a50d4123b6da
kara Odin <nihatmahdi@gmail.com>
2019-03-18 13:32:30 +0100

    second commit

--- config/filesystems.php
+++ config/filesystems.php
@@ -51,7 +51,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('http://localhost:8000').'/storage',
+            'url' => env('http://localhost').'/storage',
             'visibility' => 'public',
         ],
 
